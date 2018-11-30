9db41bea029dcb1553f87457381e2c84c7042b1b
Adriano Margarin <adriano.margarin@gmail.com>
2018-11-30 16:51:48 -0200

    Removido deleção logica e adicionado novo campo no base model

--- project_name/core/models.py
+++ project_name/core/models.py
@@ -1,9 +1,8 @@
+from uuid import uuid4
+
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
-
-from project_name.core.signals import post_soft_delete
-from project_name.core.manager import Manager
 
 
 class AbstractBaseModel(models.Model):
@@ -11,12 +10,6 @@ class AbstractBaseModel(models.Model):
     class Meta:
         abstract = True
 
+    uuid = models.UUIDField(verbose_name='UUID', default=uuid4, editable=False, unique=True)
     created_at = models.DateTimeField(verbose_name=_('Created at'), auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name=_('Updated at'), auto_now=True)
-    deleted_at = models.DateTimeField(verbose_name=_('Deleted at'), null=True, blank=True)
-    objects = Manager()
-
-    def delete(self):
-        self.deleted_at = timezone.now()
-        self.save()
-        post_soft_delete.send(sender=type(self), instance=self, using=self._state.db)
--- project_name/core/signals.py
+++
@@ -1,5 +0,0 @@
-"""Module with custom signals of the MyreksAPI project."""
-from django import dispatch
-
-
-post_soft_delete = dispatch.Signal(providing_args=['instance', 'using'])
