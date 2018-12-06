3c1577c3451000d5ef19d8fa12a4d79163135c9b
Adriano Margarin <adriano.margarin@gmail.com>
2018-12-06 14:21:59 -0200

    settings

--- project_name/settings.py
+++ project_name/settings.py
@@ -19,6 +19,7 @@ from django.utils.translation import ugettext_lazy as _
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+APPEND_SLASH = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
