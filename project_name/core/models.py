03f792223a5be06ba4afddd4ab5fdbf197bab785
Adriano Margarin <adriano.margarin@gmail.com>
2019-03-25 14:44:52 -0300

    Ajustes configs e flake8

--- project_name/core/models.py
+++ project_name/core/models.py
@@ -1,7 +1,6 @@
 from uuid import uuid4
 
 from django.db import models
-from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 
--- project_name/settings.py
+++ project_name/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'auditlog.middleware.AuditlogMiddleware',
 ]
 
 ROOT_URLCONF = 'project_name.urls'
