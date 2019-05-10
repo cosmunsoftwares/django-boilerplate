2648e9b74fdd542139f5ca5a90612b097dc26c29
Adriano Margarin <adriano.margarin@gmail.com>
2019-05-10 01:56:08 -0300

    Ajustes boilerplate

--- project_name/core/models.py
+++ project_name/core/models.py
@@ -1,5 +1,7 @@
 from uuid import uuid4
 
+from auditlog.models import AuditlogHistoryField
+
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
@@ -13,6 +15,7 @@ class AbstractBaseModel(models.Model):
     class Meta:
         abstract = True
 
+    history = AuditlogHistoryField()
     uuid = models.UUIDField(verbose_name='UUID', default=uuid4, editable=False, unique=True)
     created_at = models.DateTimeField(verbose_name=_('Created at'), auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name=_('Updated at'), auto_now=True)
